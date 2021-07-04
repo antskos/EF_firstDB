9d6f7fff418bb2c2ff33f40b3349c4d48231086c
antskos@gmail.com <antskos@gmail.com>
2021-07-04 21:20:44 +0300

    Добавление  таблиц в БД

--- DBSimple_EF/Data/SongsDB.cs
+++ DBSimple_EF/Data/SongsDB.cs
@@ -11,6 +11,9 @@ namespace DBSimple_EF.Data
 {
     public class SongsDB : DbContext
     {
+        public DbSet<Track> Tracks { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
     }
 
     [Table("Tracks")]
