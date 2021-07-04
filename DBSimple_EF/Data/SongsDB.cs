3df07fe954c197a7fc3a8db0763d038ecf7f94c3
antskos@gmail.com <antskos@gmail.com>
2021-07-04 21:10:21 +0300

    создание контекста БД и сущностей

--- DBSimple_EF/Data/SongsDB.cs
+++ DBSimple_EF/Data/SongsDB.cs
@@ -7,5 +7,15 @@ using System.Threading.Tasks;
 
 namespace DBSimple_EF.Data
 {
-   
+    public class SongsDB : DbContext
+    {
+    }
+
+    public class Track
+    {
+    }
+
+    public class Artist
+    {
+    }
 }
